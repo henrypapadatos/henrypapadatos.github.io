310a677d87d4ef76b599c2d967adc5323c483c3e
henrypapadatos <63106608+henrypapadatos@users.noreply.github.com>
2025-01-26 21:51:31 +0000

    Deploying to gh-pages from @ henrypapadatos/henrypapadatos.github.io@7cbd415283970241c863d42a7a3c9246ed2fd890 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,35 +492,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%68%65%6E%72%79@%73%61%66%65%72-%61%69.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/al-folio/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/henry-papadatos", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=sR9YhcsAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-x',
+        title: 'X',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://twitter.com/HenryPapadatos", "_blank");
         },
       },{
       id: 'light-theme',
